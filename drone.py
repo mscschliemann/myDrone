b30ab8e75866841848c30fd42d79000e80c4eaed
mscschliemann <46246023+mscschliemann@users.noreply.github.com>
2020-03-20 11:41:38 +0900

    Add files via upload
    Create

---
+++ drone.py
@@ -0,0 +1,29 @@
+"""
+Class Drone
+
+Diese Klasse bildet die Drone ab.
+- Status der Drone
+- Flugmodus: MISSION, RECEIVER
+- Hauptloop
+"""
+
+class Drone():
+
+def __init__(self):
+    """ Setze die Start-Parameter """
+    """
+    Drone.MODE
+     
+    """
+    pass
+
+def fly(self):
+    """
+    Hauptloop
+    """
+    while 1:
+        """ Lese die aktuelle Position. """
+        """ Berechne die nächste Zielposition """
+        """ Berechne den Flugvektor """
+        """ Addiere Stabilisierung """
+        """ Steuere die ESCs an """
