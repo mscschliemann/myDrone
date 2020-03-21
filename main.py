6d73ce365a4730a12e428820eaec8ae67cd9a7d0
mscschliemann <46246023+mscschliemann@users.noreply.github.com>
2020-03-21 09:32:17 +0900

    Update main.py
    0.01

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 """
 main-py
 """
+from drone import Drone as DR
+from mission import Mission
+
+copter = DR()
+mission = Mission()
+
+if (mission.getMission()['default'] != "None"):
+    copter.setMode(DR.MISSION_FLIGHT_MODE)
+
+print(copter.MODE)
+print(mission.getMission()['default'])
+
+copter.calibrate()
+
+if coper.armePrecheck() == "success":
+    copter.fly()
