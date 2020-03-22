3850c2b0506f8e32f030936308a84663994c4158
mscschliemann <mschliemann@outlook.com>
2020-03-22 15:53:07 +0900

    0.012

--- drone.py
+++ drone.py
@@ -9,9 +9,15 @@ Diese Klasse bildet die Drone ab.
 # impoer ESC
 # import ACC
 # import BAR
+# import GPS
 # import Camera
-# import Gyro
+# import Gyro (opt.)
+# import Compass (opt.)
 # import Receiver
+from threading import Timer
+from datetime import datetime
+import json
+from pid import PID
 
 class Drone():
 
@@ -19,6 +25,9 @@ class Drone():
     MISSION_FLIGHT_MODE = "MISSION_FLIGHT_MODE"
 
     def __init__(self):
+
+        with open("config.json") as f:
+            self.CONFIG = json.load(f)
         """ Setze die Start-Parameter """
         self.MODE = self.RC_FLIGHT_MODE
         self.STABALIZED = True
@@ -34,21 +43,53 @@ class Drone():
         gesichert ist und der Flugbetrieb beginnen kann """
         self.ARMED = False
 
+        self.timer = Timer(self.CONFIG['FC_LOOP_RATE'], self.IMULoop)
+        self.tick = False
+        self.timer.start()
 
         print("Initialize Drone...")
 
     def setMode(self, mode):
         self.MODE = mode
 
+    def contTimer(self):
+        self.timer = Timer(self.CONFIG['FC_LOOP_RATE'], self.IMULoop)
+        self.timer.start()
+
+
+    def IMULoop(self):
+        #global tick
+        self.tick = True
+        self.contTimer()
+
+    def printSomething(self):
+        print("Drone flies...")
+        dateTimeObj = datetime.now()
+        print(dateTimeObj.hour, ':', dateTimeObj.minute, ':', dateTimeObj.second, '.', dateTimeObj.microsecond)
+        print(id(self.timer))
+
     def fly(self):
         """
         Hauptloop
         """
-        while 1:
-            print("Drone flies...")
-            break
-            """ Lese die aktuelle Position. """
-            """ Berechne die nächste Zielposition """
-            """ Berechne den Flugvektor """
-            """ Addiere Stabilisierung """
-            """ Steuere die ESCs an """
+        try:
+            while 1:
+                if self.tick:
+                    self.printSomething()
+
+                    """ Lese die aktuelle Position. """
+                    #current_position = (0,0,0)
+                    current_position = 0
+                    """ Berechne die nächste Zielposition """
+                    #target_position = (1,1,1)
+                    target_position = 1
+                    """ Berechne den Flugvektor """
+                    vector = target_position - current_position
+                    """ Addiere Stabilisierung PID """
+                    error = PID.getCorrection()
+                    vector += error
+                    """ Steuere die ESCs an """
+                    print("---->  ", vector)
+                    self.tick = False
+        except KeyboardInterrupt:
+            print("W: interrupt received, stopping…")
--- main.py
+++ main.py
@@ -11,12 +11,15 @@ if (mission.getMission()['default'] != "None"):
     copter.setMode(DR.MISSION_FLIGHT_MODE)
 
 print(copter.MODE)
+print("FC_LOOP_RATE", copter.CONFIG['FC_LOOP_RATE'])
 print(mission.getMission()['default'])
 
-copter.calibrate()
+#copter.calibrate()
 
-if coper.armePrecheck() == "success":
-    copter.fly()
+#if coper.armePrecheck() == "success":
+#    copter.fly()
+
+copter.fly()
 
 """
 """
